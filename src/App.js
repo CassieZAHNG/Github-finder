f0190a0490932db4fb3d44fe511b9aa9f4df57cd
Cassie <cassiebetter@gmail.com>
2019-11-20 17:38:31 +1100

    change div to Fragment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Fragment>
         <h1>Hello From React</h1>
-      </div>
+      </Fragment>
     );
   }
 }
