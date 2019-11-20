ca017b6badd00aa05a5df0f216eb439c45e3d4b7
Cassie <cassiebetter@gmail.com>
2019-11-20 21:55:53 +1100

    spinner

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,15 @@
+//arrow function component
+import React, { Fragment } from "react";
+import spinner from "./spinner.gif";
+
+const Spinner = () => (
+  <Fragment>
+    <img
+      src={spinner}
+      alt="Loading..."
+      style={{ width: "200px", margin: "auto", display: "block" }}
+    />
+  </Fragment>
+);
+
+export default Spinner;
